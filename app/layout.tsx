1a084f52cf45d534a9cf23ca680bf0f94758604e
saya38812 <Saya78123@gmail.com>
2025-12-31 21:58:05 +0900

    v1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-    <meta name="google-adsense-account" content="ca-pub-8115095072198081"></meta>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8115095072198081"
+     crossorigin="anonymous"></script>
     </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
